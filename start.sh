96159f3bf581d5b89170a6702947a212569fbfae
jasonharriot <157506217+jasonharriot@users.noreply.github.com>
2025-10-09 17:48:25 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,12 @@ cd $(dirname $0)
 
 echo "Entity Inventory server @ $(date)"
 
+echo "I am $(whoami)"
 
 #Load NVM here so we can use version 22 or whatever we need
 
 . ~/.nvm/nvm.sh
+
+echo "Node version $(node --version)"
 
 node index.js
