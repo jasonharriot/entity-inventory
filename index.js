6da69ec287bdd6ba3058e1bf468d83374b338468
jasonharriot <157506217+jasonharriot@users.noreply.github.com>
2025-10-15 17:21:04 -0400

    Debug tools
    Created debug tools page. Added database downloading route.

--- index.js
+++ index.js
@@ -172,6 +172,24 @@ app.get('/api/card/write/:tagid', (req, res) => {
 	res.send(idList);
 });*/
 
+app.get('/api/backup/sqlite', (req, res) => {	//Send the whole database file.
+
+	const timestamp = new Date().toISOString().slice(0, 19).replace('T', '_'.replace(':', '-'));
+
+	const filename = `db_${timestamp}.sqlite`;
+
+	console.log(`Sending database file: ${filename}`);
+
+	res.download('db.sqlite', filename, (err) => {
+		if(err){
+			console.error('There was an error sending the database file.');
+			console.error(err);
+		} else{
+			console.log('Success!');
+		}
+	});
+})
+
 app.get('/api/sheet/generate/:templatePartial/:numPages', (req, res) => {
 	/*if(!('templatePartial' in req.params) || !('numPages' in req.params)){
 		res.writeHead(400);
