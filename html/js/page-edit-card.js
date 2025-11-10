2a11eba8d8530750a37617be52ffe9c50c6d19b1
jasonharriot <157506217+jasonharriot@users.noreply.github.com>
2025-11-10 11:51:12 -0500

    Added button to autofill today's date

--- html/js/page-edit-card.js
+++ html/js/page-edit-card.js
@@ -1,8 +1,8 @@
 const buttonSaveElem = document.getElementById('buttonSave');
-
 const buttonDiscardElem = document.getElementById('buttonDiscard');
-
 const formEditElem = document.getElementById('formEdit');
+const buttonTodayElem = document.getElementById('buttonToday');
+const dateFieldElem = document.getElementById('fieldDateToday');
 
 const tagID = getTagID();
 
@@ -10,6 +10,18 @@ buttonDiscardElem.addEventListener('click', () => {
 	if(confirm('Really discard changes?')){
 		window.location.href = `view-card.html?tagid=${tagID}`
 	} 
+});
+
+buttonTodayElem.addEventListener('click', () => {
+	const isEmpty = dateFieldElem.value == '';
+
+	if(!isEmpty && !confirm(`Overwrite the existing date with today's date?`)){
+		return;
+	}
+
+	const dateToday = moment().format('YYYY-MM-DD');
+
+	dateFieldElem.value = dateToday;
 });
 
 getCard(tagID).then((card) => {
